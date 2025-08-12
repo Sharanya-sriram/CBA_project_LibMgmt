445d5c875dab4ea8fdf02fe68549c4e7d27ce0db
Deepika-9572 <deepikasuman9572@gmail.com>
2025-08-12 11:12:47 +0530

    Added darkmode and bookdetails page

--- src/api/http-common.js
+++ src/api/http-common.js
@@ -2,22 +2,30 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001", // your json-server base
+  baseURL: "http://localhost:3001", // json-server base
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// 👇 API methods
 const api = {
+  // Books
   getBooks: () => API.get("/books"),
   getBook: (id) => API.get(`/books/${id}`),
   addBook: (book) => API.post("/books", book),
   updateBook: (id, updatedBook) => API.put(`/books/${id}`, updatedBook),
   deleteBook: (id) => API.delete(`/books/${id}`),
+
+  // Copies update only
+  updateBookCopies: (id, copies) =>
+    API.patch(`/books/${id}`, { copies }),
+
+  // Issued books
   getIssuedBooks: () => API.get("/issuedBooks"),
+  addIssuedBook: (issuedBook) => API.post("/issuedBooks", issuedBook),
   deleteIssuedBook: (id) => API.delete(`/issuedBooks/${id}`),
 
+  // Users
   getUsers: () => API.get("/users"),
   getUser: (id) => API.get(`/users/${id}`),
   addUser: (user) => API.post("/users", user),
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,18 +4,20 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
+  const [darkMode, setDarkMode] = useState(true);
   const usersDB = [
-    { username: "user1", password: "password1" },
-    { username: "user2", password: "password2" },
+    { username: "user1", password: "password1",role:"admin",id:'1' },
+    { username: "user2", password: "password2" ,role:"user",id:'2'},
   ];
-
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode);
+  }
   const login = (username, password) => {
     const matchedUser = usersDB.find(
       (u) => u.username === username && u.password === password
     );
     if (matchedUser) {
-      setUser({ username: matchedUser.username });
+      setUser({ username: matchedUser.username,role:matchedUser.role });
       return true;
     } else {
       return false;
@@ -27,7 +29,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, login, logout,darkMode,toggleDarkMode }}>
       {children}
     </AuthContext.Provider>
   );
